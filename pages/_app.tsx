3fd5716eb7b5b75cbe9745f20d604a9ed3c9d903
rize-ma <masaki030603@gmail.com>
2023-05-04 00:11:02 +0900

    セットアップ

---
+++ pages/_app.tsx
@@ -0,0 +1,36 @@
+import '@/styles/globals.css'
+import { useEffect } from 'react'
+import type { AppProps } from 'next/app'
+import { MantineProvider } from '@mantine/core'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
+import axios from 'axios'
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      retry: false,
+      refetchOnWindowFocus: false,
+    },
+  },
+})
+
+export default function App({ Component, pageProps }: AppProps) {
+  axios.defaults.withCredentials = true
+
+  return (
+    <QueryClientProvider client={queryClient}>
+      <MantineProvider
+        withGlobalStyles
+        withNormalizeCSS
+        theme={{
+          colorScheme: 'dark',
+          fontFamily: 'Verdana, sans-serif',
+        }}
+      >
+        <Component {...pageProps} />
+      </MantineProvider>
+      <ReactQueryDevtools />
+    </QueryClientProvider>
+  )
+}
