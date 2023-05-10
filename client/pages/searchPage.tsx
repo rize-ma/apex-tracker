4d9848b5ac6eefe345ab8892c21a0d8ee87e5277
rize-ma <masaki030603@gmail.com>
2023-05-10 09:18:14 +0900

    ユーザー検索に失敗した際の処理を追加

--- client/pages/searchPage.tsx
+++ client/pages/searchPage.tsx
@@ -3,20 +3,36 @@ import { PlatformTab } from '@/components/PlatformTab'
 import { SearchBox } from '@/components/SearchBox'
 import { SubmitButton } from '@/components/SubmitButton'
 import { searchUser } from '@/utils/searchUser'
+import { Alert } from '@mantine/core'
 import { NextPage } from 'next'
 import { FormEvent, useState } from 'react'
 
 export const SearchPage: NextPage = () => {
   const [platform, setPlatform] = useState('origin')
   const [inputValue, setInputValue] = useState('')
+  const [errorMessage, setErrorMessage] = useState('')
 
   const handleSubmit = async (e: FormEvent<HTMLFormElement>) => {
     e.preventDefault()
     const data = await searchUser(platform, inputValue)
-    console.log(data)
+    if (data.message) {
+      setErrorMessage(data.message)
+      return
+    }
   }
   return (
     <Layout title="ユーザー検索">
+      {errorMessage && (
+        <Alert
+          my="md"
+          variant="filled"
+          title="検索失敗"
+          color="red"
+          radius="md"
+        >
+          {errorMessage}
+        </Alert>
+      )}
       <form onSubmit={handleSubmit}>
         <div className="flex flex-col items-center justify-center">
           <PlatformTab platform={platform} setPlatform={setPlatform} />
--- client/utils/searchUser.ts
+++ client/utils/searchUser.ts
@@ -2,14 +2,10 @@ import axios from 'axios'
 
 export const searchUser = (platform: string, userId: string) => {
   const getUserData = () => {
-    try {
-      const data = axios.get(
-        `http://localhost:3005/apex-tracker/${platform}/${userId}`
-      )
-      return data
-    } catch (e: any) {
-      return { message: 'エラーが発生しました' }
-    }
+    const data = axios
+      .get(`http://localhost:3005/apex-tracker/${platform}/${userId}`)
+      .then((value) => value.data)
+    return data
   }
   return getUserData()
 }
