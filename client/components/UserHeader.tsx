3a76bef4e6c15f457c430e7b9f95e88b53bcd2da
rize-ma <masaki030603@gmail.com>
2023-05-20 15:19:21 +0900

    ヘッダー表示のロジックを修正

--- client/components/UserHeader.tsx
+++ client/components/UserHeader.tsx
@@ -4,8 +4,18 @@ import Image from 'next/image'
 
 export const UserHeader: FC = () => {
   const { userData } = useStore()
+  const getLegendHeader = () => {
+    const activeLegendId = userData.metadata.activeLegend
+    let legendHeader
+    userData.segments.forEach((data) => {
+      if (data.attributes.id === activeLegendId) {
+        legendHeader = data.metadata.bgImageUrl
+      }
+    })
+    return legendHeader
+  }
   const imageHeaderUrl =
-    userData.segments[1]?.metadata.bgImageUrl ||
+    getLegendHeader() ||
     'https://trackercdn.com/cdn/apex.tracker.gg/legends/wraith-concept-bg-small.jpg'
   return (
     <div>
