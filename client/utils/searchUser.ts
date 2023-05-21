81db245bbf2779a9b46fb9540266b9d836e2d9e7
rize-ma <masaki030603@gmail.com>
2023-05-21 15:40:38 +0900

    deploy設定

--- client/utils/searchUser.ts
+++ client/utils/searchUser.ts
@@ -3,7 +3,9 @@ import axios from 'axios'
 export const searchUser = (platform: string, userId: string) => {
   const getUserData = () => {
     const data = axios
-      .get(`http://localhost:3005/apex-tracker/${platform}/${userId}`)
+      .get(
+        `${process.env.NEXT_PUBLIC_BASE_URL}/apex-tracker/${platform}/${userId}`
+      )
       .then((value) => value.data.data)
     return data
   }
