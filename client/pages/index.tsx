a313a377422aee5edf1dea751203985827407681
rize-ma <masaki030603@gmail.com>
2023-06-11 12:26:41 +0900

    初回アクセス時にAPIにリクエストを送るように設定

--- client/pages/index.tsx
+++ client/pages/index.tsx
@@ -1,6 +1,11 @@
+import { useEffect } from 'react'
 import SearchPage from './searchPage'
+import { initialAccess } from '@/utils/initialAccess'
 
 export default function Home() {
+  useEffect(() => {
+    initialAccess()
+  }, [])
   return (
     <div>
       <SearchPage />
